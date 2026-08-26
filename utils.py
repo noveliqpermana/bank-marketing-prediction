529f4b2b0d1cc9a8719b16034b77925c4bfa2f09
noveliqpermana <noveliq.permana@gmail.com>
2026-08-26 19:20:21 +0700

    commit 5

--- utils.py
+++ utils.py
@@ -1,7 +1,12 @@
 def cardinality(X):
+    '''Fungsi ini berguna untuk melakukan handling cardinality serta sekaligus menambah indikator untuk kolom pdays dan mengubah nilai -1'''
+    # lakukan copy agar tidak mempengaruhi data asli
     X = X.copy()
+
+    # buat list untuk menampung data
     job_list = []
 
+    # lakukan for looping untuk kategorisasi job
     for i in X['job']:
         if i in ['unemployed', 'student', 'retired']:
             job_list.append('not_working')
@@ -14,8 +19,14 @@ def cardinality(X):
         else:
             job_list.append(i)
 
-    X['was_contacted'] = (X['pdays'] != -1).astype(int)
-    X['pdays'] = X['pdays'].where(X['pdays'] != -1, 0) 
+    # masukkan data ke kolom job
     X['job'] = job_list
 
+    # buat indikator untuk leads yang tidak dihubungi 
+    X['was_contacted'] = (X['pdays'] != -1).astype(int)
+
+    # ganti nilai -1 menjadi 0
+    X['pdays'] = X['pdays'].where(X['pdays'] != -1, 0) 
+
+    # return data baru yang sudah di-handling
     return X
